479c818d0f42ce8987e43c8028c0dbabdb835ba1
2dam <2dam@MACINF04.TartangaLH.eus>
2019-10-22 08:57:45 +0200

    FIRST CHAPUZA

--- src/retoLogin/view/FXMLDocumentControllerLogin.java
+++ src/retoLogin/view/FXMLDocumentControllerLogin.java
@@ -7,7 +7,17 @@ package retoLogin.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,13 +25,54 @@ import javafx.fxml.Initializable;
  * @author 2dam
  */
 public class FXMLDocumentControllerLogin implements Initializable {
-
+    @FXML
+    private Label lblLogin;
+    @FXML
+    private Label lblPassword;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Button btnSignUp;
+    @FXML
+    private TextField txtFieldLogin;
+    @FXML
+    private TextField txtFieldPassword;
+    
+    
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void initialize(URL location, ResourceBundle resources) {
+        btnLogin.setOnAction(this::handleButtonAction);
+        addTextLimiter(txtFieldLogin, 30);
+    }
+    
+    
+    private void handleButtonAction(ActionEvent e){
+        if(txtFieldLogin.getText().length()>30){
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Invalid Username");
+            alert.setHeaderText(null);
+            alert.setContentText("You must enter a valid username.");
+
+            alert.showAndWait();
+        }else{
+            //PUES ESO, QUE INTENTE LOGEAR Y ESA MOVIDA
+        }
+    }
+
+    public static void addTextLimiter(final TextField tf, final int maxLength) {
+    tf.textProperty().addListener(new ChangeListener<String>() {
+        @Override
+        public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+            if (tf.getText().length() > maxLength) {
+                String s = tf.getText().substring(0, maxLength);
+                tf.setText(s);
+            }
+        }
+    });
+}
+ 
     
 }

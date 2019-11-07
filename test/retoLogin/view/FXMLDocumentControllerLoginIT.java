4e77fee1dcbfc7ac927e564ea80c73550e39f297
Daira <2dam@MACINF04.TartangaLH.eus>
2019-11-07 08:38:26 +0100

    integration test dor the login

--- test/retoLogin/view/FXMLDocumentControllerLoginIT.java
+++ test/retoLogin/view/FXMLDocumentControllerLoginIT.java
@@ -5,27 +5,23 @@
  */
 package retoLogin.view;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.stage.Stage;
-import org.junit.After;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Before;
+import org.junit.runners.MethodSorters;
 import static org.testfx.api.FxAssert.verifyThat;
 import org.testfx.framework.junit.ApplicationTest;
 import static org.testfx.matcher.base.NodeMatchers.isVisible;
 import retoLogin.Application;
 
 /**
- *
+ * The integration test for the FXMLDocumentControllerLogin.
  * @author Daira Eguzkiza
  */
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FXMLDocumentControllerLoginIT extends ApplicationTest{
     private static final String INVALIDUSERNAME="user$name·?%";
-    private static final String VALIDUSERNAME="username1234";
     private static final String LONGUSERNAME="dkufbiuapbdfiabfvibfvivfirfbie"
             + "aaasdiufbaoisdbiaoslbdfaisdfaa"
             + "duhfaildfhgioadgbiladfbviadbvi"
@@ -45,12 +41,12 @@ public class FXMLDocumentControllerLoginIT extends ApplicationTest{
     @Override public void start(Stage stage) throws Exception {
       new Application().start(stage);
     }
-    //VERIFY THAT XpANE IS VISIBLE()
-    //Assert.assertNotNull();
     
-    
+    /** 
+     * Tests if everything goes right when you enter valid parameters.
+     */
     @Test
-    public void testLoginButtonIfOk() {
+    public void testZLoginButtonIfOk() {
         clickOn("#txtFieldLogin");
         write(USEREXISTS);
         clickOn("#txtFieldPassword");
@@ -59,63 +55,109 @@ public class FXMLDocumentControllerLoginIT extends ApplicationTest{
         verifyThat("Welcome, Jon Calvo Gaminde", isVisible());
     }
     
+    /**
+     * Tests if when you try to log in without password, the application shows
+     * an alert dialog saying you must enter a password/user.
+     */
     @Test
-    public void testLoginButtonWithoutPasswd() {
+    public void testALoginButtonWithoutPasswd() {
         clickOn("#txtFieldLogin");
         write(USEREXISTS);
         clickOn("#txtFieldPassword");
         write("");
         clickOn("#btnLogin");
         verifyThat("You must enter a username and a password.", isVisible());
-           
+        clickOn("Aceptar");
     }
     
+    /**
+     * Tests if when you try to log in with a non existent user the 
+     * application shows an alert dialog saying that the user doesn't exist.
+     */
     @Test
-    public void testLoginButtonNonExistingUser() {
+    public void testBLoginButtonNonExistingUser() {
         clickOn("#txtFieldLogin");
         write(NONEXISTENTUSER);
         clickOn("#txtFieldPassword");
         write(PASSWORD);
         clickOn("#btnLogin");
         verifyThat("That user does not exist.", isVisible());
+        clickOn("Aceptar");
     }
     
-    //@Test
-    public void testLoginButtonWithoutUser() {
+    /**
+     * Tests if when you try to log in without entering a username the 
+     * application shows an alert dialog saying that you must enter a 
+     * username/password.
+     */
+    @Test
+    public void testCLoginButtonWithoutUser() {
         clickOn("#txtFieldLogin");
         write("");
         clickOn("#txtFieldPassword");
         write(PASSWORD);
         clickOn("#btnLogin");
         verifyThat("You must enter a username and a password.", isVisible());
+        clickOn("Aceptar");
     }
     
+    /**
+     * Tests if when you try to log in with an invalid username the 
+     * application shows an alert dialog saying you must enter a valid
+     * username.
+     */
     @Test
-    public void testLoginButtonWithWrongPassw() {
+    public void testDLoginButtonWithInvalidUsername() {
+        clickOn("#txtFieldLogin");
+        write(INVALIDUSERNAME);
+        clickOn("#txtFieldPassword");
+        write(PASSWORD);
+        clickOn("#btnLogin");
+        verifyThat("You must enter a valid username.", isVisible());
+        clickOn("Aceptar");
+    }
+    
+    /**
+     * Tests if when you try to log in with a wrong password the application
+     * shows an alert dialog saying that the password is not correct.
+     */
+    @Test
+    public void testELoginButtonWithWrongPassw() {
         clickOn("#txtFieldLogin");
         write(USEREXISTS);
         clickOn("#txtFieldPassword");
         write(PASSWORD);
         clickOn("#btnLogin");
         verifyThat("The password you have entered is not correct.", isVisible());
+        clickOn("Aceptar");
     }
     
+    /**
+     * Tests if when you try to enter a really long username it reaches a point
+     * where you can't write more, but if for some reason this fails, it must
+     * show an alert dialog saying that you must enter a valid username).
+     */
    @Test
-    public void testLoginButtonWithMastodonteUsername() {
+    public void testFLoginButtonWithMastodonteUsername() {
         clickOn("#txtFieldLogin");
         write(LONGUSERNAME);
         clickOn("#txtFieldPassword");
         write(PASSWORD);
         clickOn("#btnLogin");
-        verifyThat("That user does not exist.", isVisible());
+        if("#txtFieldLogin".length()>50){
+           verifyThat("You must enter a valid username.", isVisible());
+        }
+        clickOn("Aceptar");
     }
     
+    /**
+     * Tests if when you click on the sign up button, another window is opened
+     * showing the sign up options.
+     */
    @Test
-    public void testSignUpBtn() {
+    public void testGSignUpBtn() {
         clickOn("#btnSignUp");
         verifyThat("Sign Up", isVisible());
-        
+        clickOn("#btnCancel");
     }
-    
-    
 }

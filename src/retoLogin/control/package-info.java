d9426288bcc7be6609c762fa76d4e0659408603a
Daira <2dam@MACINF04.TartangaLH.eus>
2019-11-14 10:13:12 +0100

    javadoc

--- src/retoLogin/control/package-info.java
+++ src/retoLogin/control/package-info.java
@@ -1,6 +1,8 @@
 /**
  * This packages creates the client to send them to the user in case they need it.
  * It consists in an interface, a factory and the implementation.
+ * 
+ * @author Daira Eguzkiza
  */
 package retoLogin.control;
 
--- src/retoLogin/view/package-info.java
+++ src/retoLogin/view/package-info.java
@@ -2,5 +2,7 @@
  * This is the package that manages all the windows for the user to interact with.
  * It consists in the login, the registration, the sign out window and a tutorial/help
  * window.
+ * 
+ * @author Daira Eguzkiza
  */
 package retoLogin.view;
